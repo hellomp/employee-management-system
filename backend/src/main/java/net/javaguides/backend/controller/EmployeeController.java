89190fdde385e51308e27945e25685eab8410f50
Marcos Paulo <mpcc.show@gmail.com>
2021-10-29 16:26:06 -0300

    Implements Employee controller

---
+++ backend/src/main/java/net/javaguides/backend/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package net.javaguides.backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.javaguides.backend.model.Employee;
+import net.javaguides.backend.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+  @Autowired
+  private EmployeeRepository employeeRepository;
+
+  @GetMapping("/employees")
+  public List<Employee> getAllEmployees(){
+    return employeeRepository.findAll();
+  }
+}
